9bbeb4508cdf442701d9028da71160454e95a7b1
shyam more <skumarmoure@gmail.com>
2024-05-22 19:24:57 +0530

    hell

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { tabs } from "./tabs";
 
 function App() {
+  let [activeData, setactiveData] = useState(0);
+  let [activeContant, setactiveContant] = useState(tabs[0]);
+  let changeData = (index) => {
+    setactiveData(index);
+    setactiveContant(tabs[index]);
+  };
+
+  let [todolist, settodolist] = useState([]);
+
+  let list = todolist.map((value, index) => {
+    return (
+      <ToDoListItem
+        value={value}
+        key={index}
+        indexNumber={index}
+        todolist={todolist}
+        settodolist={settodolist}
+      />
+    );
+  });
+
+  let SaveToDoList = (event) => {
+    let toname = event.target.toname.value;
+    if (!todolist.includes(toname)) {
+      let FinalToDoList = [...todolist, toname];
+      settodolist(FinalToDoList);
+    } else {
+      alert("ToDoname Already Exist...... ");
+    }
+    event.preventDefault();
+  };
+
+  // html page start
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="tabsOuter">
+        <h1>tabs management system</h1>
+        <div>
+          <ul>
+            {tabs.map((tabsItem, index) => {
+              return (
+                <li>
+                  <button
+                    onClick={() => changeData(index)}
+                    className={activeData == index ? "activeButton" : ""}
+                  >
+                    {tabsItem.title}
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+          {activeContant !== undefined ? <p>{activeContant.detail}</p> : ""}
+        </div>
+      </div>
+
+      <h1>ToDoList</h1>
+      <form onSubmit={SaveToDoList}>
+        <input type="text" name="toname" /> <button>save</button>
+      </form>
+
+      <div className="Overlay">
+        <ul>{list}</ul>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function ToDoListItem({ value, indexNumber, todolist, settodolist }) {
+  let [status, setstatus] = useState(false);
+
+  let deleteRow = () => {
+    let finalData = todolist.filter((v, i) => {
+      return i != indexNumber;
+    });
+    settodolist(finalData);
+  };
+
+  let cheakStatus = () => {
+    setstatus(!status);
+  };
+  return (
+    <li className={status ? "activetab" : ""} onClick={cheakStatus}>
+      {value} <span onClick={deleteRow}>&times;</span>
+    </li>
+  );
+}
---
+++ src/tabs.jsx
@@ -0,0 +1,14 @@
+export let tabs = [
+  {
+    title: "mission",
+    detail: "it is very important task for oure team",
+  },
+  {
+    title: "vision",
+    detail: "in aramy vision team member eleminet the all enemy",
+  },
+  {
+    title: "about ",
+    detail: "i am the commander of team which lead the hole mission ",
+  },
+];
